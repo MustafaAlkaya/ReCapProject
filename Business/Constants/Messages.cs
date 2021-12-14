cce19f4575e4ef6dd3515428eeaa0c756c64adcc
Mustafa Alkaya <info.mustafaalkaya@gmail.com>
2021-12-14 19:32:56 +0300

    XX

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,7 +10,7 @@ namespace Business.Constants
         public static string Listed = "Listeleme işlemi başarılı";
         public static string Updated = "Güncelleme işlemi başarılı";
         public static string Deleted = "Silme işlemi başarılı";
-        public static string CarAdded = "Araç başarıyla eklendi.";
+        public static string CarAdded = "Araç başarıyla eklendi22.";
         public static string InvalidCarDetail = "Araç ismi 2 karakterden uzun olmalı ve günlük bedeli 0'dan büyük olmalıdır.";
         public static string InvalidCustomerName = "Şirket adı 2 karakterden uzun olmalıdır.";
         public static string CarsListed = "Araçlar listelendi";
